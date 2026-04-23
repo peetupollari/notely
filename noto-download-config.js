cd112e2c8534a7aa06a497f90e0f852f35997468
peetupollari <peetupollari@users.noreply.github.com>
2026-04-23 20:59:45 +0300

    deleted:    noto-download-config.js 	deleted:    noto-download.css 	deleted:    noto-download.html 	deleted:    noto-download.js 	modified:   noto.css 	modified:   noto.html 	modified:   noto.js 	deleted:    supabase/functions/_shared/purchases.ts 	deleted:    supabase/functions/_shared/stripe.ts 	deleted:    supabase/functions/create-download-link/index.ts 	deleted:    supabase/functions/stripe-sync-session/index.ts 	deleted:    supabase/functions/stripe-webhook/index.ts 	modified:   supabase/noto-paid-download.sql

--- noto-download-config.js
+++
@@ -1,7 +0,0 @@
-window.NOTO_DOWNLOAD_GATE_CONFIG = {
-    siteUrl: "https://www.notely.uk",
-    downloadPagePath: "/noto-download.html",
-    stripePaymentLink: "https://buy.stripe.com/test_eVqfZa4673pK35H7rogw001",
-    supportEmail: "support@notely.uk",
-    ...window.NOTO_DOWNLOAD_GATE_CONFIG
-};
--- supabase/functions/_shared/stripe.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from "https://esm.sh/stripe@14?target=denonext";
-import { requireEnv } from "./supabase.ts";
-
-export const stripe = new Stripe(requireEnv("STRIPE_SECRET_KEY"), {
-    apiVersion: "2024-11-20"
-});
-
-export const stripeCryptoProvider = Stripe.createSubtleCryptoProvider();
